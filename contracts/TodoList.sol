bf5b6723cb5a108038b715c61dc9180d0ac06827
AnirudhR <52647011+Anirudh-R-1201@users.noreply.github.com>
2020-05-14 21:22:25 +0530

    3.

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,22 @@ pragma solidity >=0.4.21 <0.7.0;
 
 contract TodoList{
     uint public taskCount = 0;
-    
+    mapping (uint=>Task) public tasks; 
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    constructor() public{
+        createTask("Test");
+    }
+
+    function createTask(string memory content) public{
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, content, false);
+    }
+
+
 }
